9432d7e7bfb5b22dddff05541324b94e476cfa2d
Riley Labrecque <git@rileylabrecque.com>
2013-12-21 09:10:43 -0600

    Added a second scene to test switching between scenes

---
+++ Assets/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwitcher : MonoBehaviour {
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.F5)) {
+			if (Application.loadedLevel == 0) {
+				Application.LoadLevel(1);
+			}
+			else {
+				Application.LoadLevel(0);
+			}
+		}
+	}
+}
