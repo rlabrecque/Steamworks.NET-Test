2bd282f05b121d84c6de503ce0c6fba8d73dc3a8
Riley Labrecque <git@rileylabrecque.com>
2015-05-07 09:46:57 -0600

    Added new tests from Steamworks SDK 1.33 - SteamUGC

--- Assets/Scripts/SteamUGCTest.cs
+++ Assets/Scripts/SteamUGCTest.cs
@@ -8,22 +8,26 @@ public class SteamUGCTest : MonoBehaviour {
 	private UGCUpdateHandle_t m_UGCUpdateHandle;
 
 	protected Callback<ItemInstalled_t> m_ItemInstalled;
+	protected Callback<DownloadItemResult_t> m_DownloadItemResult;
 
 	private CallResult<SteamUGCQueryCompleted_t> OnSteamUGCQueryCompletedCallResult;
 	private CallResult<SteamUGCRequestUGCDetailsResult_t> OnSteamUGCRequestUGCDetailsResultCallResult;
 	private CallResult<CreateItemResult_t> OnCreateItemResultCallResult;
 	private CallResult<SubmitItemUpdateResult_t> OnSubmitItemUpdateResultCallResult;
+	private CallResult<UserFavoriteItemsListChanged_t> OnUserFavoriteItemsListChangedCallResult;
 
 	private CallResult<RemoteStorageSubscribePublishedFileResult_t> OnRemoteStorageSubscribePublishedFileResultCallResult;
 	private CallResult<RemoteStorageUnsubscribePublishedFileResult_t> OnRemoteStorageUnsubscribePublishedFileResultCallResult;
 
 	public void OnEnable() {
 		m_ItemInstalled = Callback<ItemInstalled_t>.Create(OnItemInstalled);
+		m_DownloadItemResult = Callback<DownloadItemResult_t>.Create(OnDownloadItemResult);
 
 		OnSteamUGCQueryCompletedCallResult = CallResult<SteamUGCQueryCompleted_t>.Create(OnSteamUGCQueryCompleted);
 		OnSteamUGCRequestUGCDetailsResultCallResult = CallResult<SteamUGCRequestUGCDetailsResult_t>.Create(OnSteamUGCRequestUGCDetailsResult);
 		OnCreateItemResultCallResult = CallResult<CreateItemResult_t>.Create(OnCreateItemResult);
 		OnSubmitItemUpdateResultCallResult = CallResult<SubmitItemUpdateResult_t>.Create(OnSubmitItemUpdateResult);
+		OnUserFavoriteItemsListChangedCallResult = CallResult<UserFavoriteItemsListChanged_t>.Create(OnUserFavoriteItemsListChanged);
 
 		// These come from ISteamRemoteStorage but they are used here as well...
 		OnRemoteStorageSubscribePublishedFileResultCallResult = CallResult<RemoteStorageSubscribePublishedFileResult_t>.Create(OnRemoteStorageSubscribePublishedFileResult);
@@ -48,6 +52,15 @@ public class SteamUGCTest : MonoBehaviour {
 			print("SteamUGC.CreateQueryAllUGCRequest(EUGCQuery.k_EUGCQuery_RankedByPublicationDate, EUGCMatchingUGCType.k_EUGCMatchingUGCType_AllGuides, " + AppId_t.Invalid + ", " + SteamUtils.GetAppID() + ", 1) : " + m_UGCQueryHandle);
 		}
 
+		if (GUILayout.Button("CreateQueryUGCDetailsRequest(PublishedFileIDs, (uint)PublishedFileIDs.Length)")) {
+			PublishedFileId_t[] PublishedFileIDs = new PublishedFileId_t[] {
+				(PublishedFileId_t)280762427,
+				//(PublishedFileId_t)280664702,
+			};
+			m_UGCQueryHandle = SteamUGC.CreateQueryUGCDetailsRequest(PublishedFileIDs, (uint)PublishedFileIDs.Length);
+			print("SteamUGC.CreateQueryUGCDetailsRequest(PublishedFileIDs, " + (uint)PublishedFileIDs.Length + ") : " + m_UGCQueryHandle);
+		}
+
 		if (GUILayout.Button("SendQueryUGCRequest(m_UGCQueryHandle)")) {
 			SteamAPICall_t handle = SteamUGC.SendQueryUGCRequest(m_UGCQueryHandle);
 			OnSteamUGCQueryCompletedCallResult.Set(handle);
@@ -59,6 +72,41 @@ public class SteamUGCTest : MonoBehaviour {
 			bool ret = SteamUGC.GetQueryUGCResult(m_UGCQueryHandle, 0, out Details);
 			print("SteamUGC.GetQueryUGCResult(" + m_UGCQueryHandle + ", 0, out Details) : " + ret);
 			print(Details.m_nPublishedFileId + " -- " + Details.m_eResult + " -- " + Details.m_eFileType + " -- " + Details.m_nCreatorAppID + " -- " + Details.m_nConsumerAppID + " -- " + Details.m_rgchTitle + " -- " + Details.m_rgchDescription + " -- " + Details.m_ulSteamIDOwner + " -- " + Details.m_rtimeCreated + " -- " + Details.m_rtimeUpdated + " -- " + Details.m_rtimeAddedToUserList + " -- " + Details.m_eVisibility + " -- " + Details.m_bBanned + " -- " + Details.m_bAcceptedForUse + " -- " + Details.m_bTagsTruncated + " -- " + Details.m_rgchTags + " -- " + Details.m_hFile + " -- " + Details.m_hPreviewFile + " -- " + Details.m_pchFileName + " -- " + Details.m_nFileSize + " -- " + Details.m_nPreviewFileSize + " -- " + Details.m_rgchURL + " -- " + Details.m_unVotesUp + " -- " + Details.m_unVotesDown + " -- " + Details.m_flScore + " -- " + Details.m_unNumChildren);
+		}
+
+		if (GUILayout.Button("GetQueryUGCPreviewURL(m_UGCQueryHandle, 0, out URL, 1024)")) {
+			string URL;
+			bool ret = SteamUGC.GetQueryUGCPreviewURL(m_UGCQueryHandle, 0, out URL, 1024);
+			print("SteamUGC.GetQueryUGCPreviewURL(" + m_UGCQueryHandle + ", 0, out URL, 1024) : " + ret + " -- " + URL);
+		}
+
+		if (GUILayout.Button("GetQueryUGCMetadata(m_UGCQueryHandle, 0, out Metadata, Constants.k_cchDeveloperMetadataMax)")) {
+			string Metadata;
+			bool ret = SteamUGC.GetQueryUGCMetadata(m_UGCQueryHandle, 0, out Metadata, Constants.k_cchDeveloperMetadataMax);
+			print("SteamUGC.GetQueryUGCMetadata(" + m_UGCQueryHandle + ", 0, out Metadata, " + Constants.k_cchDeveloperMetadataMax + ") : " + ret + " -- " + Metadata);
+		}
+
+		if (GUILayout.Button("GetQueryUGCChildren(m_UGCQueryHandle, 0, PublishedFileIDs, (uint)PublishedFileIDs.Length)")) {
+			PublishedFileId_t[] PublishedFileIDs = new PublishedFileId_t[1]; // Use SteamUGCDetails_t::m_unNumChildren instead...
+			bool ret = SteamUGC.GetQueryUGCChildren(m_UGCQueryHandle, 0, PublishedFileIDs, (uint)PublishedFileIDs.Length);
+			print("SteamUGC.GetQueryUGCChildren(" + m_UGCQueryHandle + ", 0, PublishedFileIDs, " + (uint)PublishedFileIDs.Length + ") : " + ret + " -- " + PublishedFileIDs[0]);
+		}
+
+		if (GUILayout.Button("GetQueryUGCStatistic(m_UGCQueryHandle, 0, EItemStatistic.k_EItemStatistic_NumSubscriptions, out StatValue)")) {
+			uint StatValue;
+			bool ret = SteamUGC.GetQueryUGCStatistic(m_UGCQueryHandle, 0, EItemStatistic.k_EItemStatistic_NumSubscriptions, out StatValue);
+			print("SteamUGC.GetQueryUGCStatistic(" + m_UGCQueryHandle + ", 0, EItemStatistic.k_EItemStatistic_NumSubscriptions, out StatValue) : " + ret + " -- " + StatValue);
+		}
+
+		if (GUILayout.Button("GetQueryUGCNumAdditionalPreviews(m_UGCQueryHandle, 0)")) {
+			print("SteamUGC.GetQueryUGCNumAdditionalPreviews(" + m_UGCQueryHandle + ", 0) : " + SteamUGC.GetQueryUGCNumAdditionalPreviews(m_UGCQueryHandle, 0));
+		}
+
+		if (GUILayout.Button("GetQueryUGCAdditionalPreview(m_UGCQueryHandle, 0, 0, out URLOrVideoID, 1024, out IsImage)")) {
+			string URLOrVideoID;
+			bool IsImage;
+			bool ret = SteamUGC.GetQueryUGCAdditionalPreview(m_UGCQueryHandle, 0, 0, out URLOrVideoID, 1024, out IsImage); // Should check GetQueryUGCNumAdditionalPreviews first.
+			print("SteamUGC.GetQueryUGCAdditionalPreview(" + m_UGCQueryHandle + ", 0, 0, out URLOrVideoID, 1024, out IsImage) : " + ret + " -- " + URLOrVideoID + " -- " + IsImage);
 		}
 
 		if (GUILayout.Button("ReleaseQueryUGCRequest(m_UGCQueryHandle)")) {
@@ -76,7 +124,19 @@ public class SteamUGCTest : MonoBehaviour {
 		if (GUILayout.Button("SetReturnLongDescription(m_UGCQueryHandle, true)")) {
 			print("SteamUGC.SetReturnLongDescription(" + m_UGCQueryHandle + ", true) : " + SteamUGC.SetReturnLongDescription(m_UGCQueryHandle, true));
 		}
-	
+
+		if (GUILayout.Button("SetReturnMetadata(m_UGCQueryHandle, true)")) {
+			print("SteamUGC.SetReturnMetadata(" + m_UGCQueryHandle + ", true) : " + SteamUGC.SetReturnMetadata(m_UGCQueryHandle, true));
+		}
+
+		if (GUILayout.Button("SetReturnChildren(m_UGCQueryHandle, true)")) {
+			print("SteamUGC.SetReturnChildren(" + m_UGCQueryHandle + ", true) : " + SteamUGC.SetReturnChildren(m_UGCQueryHandle, true));
+		}
+
+		if (GUILayout.Button("SetReturnAdditionalPreviews(m_UGCQueryHandle, true)")) {
+			print("SteamUGC.SetReturnAdditionalPreviews(" + m_UGCQueryHandle + ", true) : " + SteamUGC.SetReturnAdditionalPreviews(m_UGCQueryHandle, true));
+		}
+			
 		if (GUILayout.Button("SetReturnTotalOnly(m_UGCQueryHandle, true)")) {
 			print("SteamUGC.SetReturnTotalOnly(" + m_UGCQueryHandle + ", true) : " + SteamUGC.SetReturnTotalOnly(m_UGCQueryHandle, true));
 		}
@@ -128,6 +188,11 @@ public class SteamUGCTest : MonoBehaviour {
 			print("SteamUGC.SetItemDescription(" + m_UGCUpdateHandle + ", \"This is the test description.\") : " + ret);
 		}
 
+		if (GUILayout.Button("SetItemMetadata(m_UGCUpdateHandle, \"This is the test metadata.\")")) {
+			bool ret = SteamUGC.SetItemMetadata(m_UGCUpdateHandle, "This is the test metadata.");
+			print("SteamUGC.SetItemMetadata(" + m_UGCUpdateHandle + ", \"This is the test metadata.\") : " + ret);
+		}
+
 		if (GUILayout.Button("SetItemVisibility(m_UGCUpdateHandle, ERemoteStoragePublishedFileVisibility.k_ERemoteStoragePublishedFileVisibilityPrivate)")) {
 			bool ret = SteamUGC.SetItemVisibility(m_UGCUpdateHandle, ERemoteStoragePublishedFileVisibility.k_ERemoteStoragePublishedFileVisibilityPrivate);
 			print("SteamUGC.SetItemVisibility(" + m_UGCUpdateHandle + ", ERemoteStoragePublishedFileVisibility.k_ERemoteStoragePublishedFileVisibilityPrivate) : " + ret);
@@ -161,16 +226,28 @@ public class SteamUGCTest : MonoBehaviour {
 			GUILayout.Label("GetItemUpdateProgress(m_UGCUpdateHandle, out BytesProcessed, out BytesTotal) : " + ret + " -- " + BytesProcessed + " -- " + BytesTotal);
 		}
 
-		if (GUILayout.Button("SubscribeItem((PublishedFileId_t)113142309)")) {
-			SteamAPICall_t handle = SteamUGC.SubscribeItem((PublishedFileId_t)113142309); // http://steamcommunity.com/sharedfiles/filedetails/?id=113142309
-			OnRemoteStorageSubscribePublishedFileResultCallResult.Set(handle);
-			print("SteamUGC. : " + handle);
+		if (GUILayout.Button("AddItemToFavorites(SteamUtils.GetAppID(), (PublishedFileId_t)280762427)")) {
+			SteamAPICall_t handle = SteamUGC.AddItemToFavorites(SteamUtils.GetAppID(), (PublishedFileId_t)280762427); // http://steamcommunity.com/sharedfiles/filedetails/?id=280762427
+			OnUserFavoriteItemsListChangedCallResult.Set(handle);
+			print("SteamUGC.AddItemToFavorites(" + SteamUtils.GetAppID() + ", (PublishedFileId_t)280762427) : " + handle);
 		}
 
-		if (GUILayout.Button("UnsubscribeItem((PublishedFileId_t)113142309)")) {
-			SteamAPICall_t handle = SteamUGC.UnsubscribeItem((PublishedFileId_t)113142309); // http://steamcommunity.com/sharedfiles/filedetails/?id=113142309
+		if (GUILayout.Button("RemoveItemFromFavorites(SteamUtils.GetAppID(), (PublishedFileId_t)280762427)")) {
+			SteamAPICall_t handle = SteamUGC.RemoveItemFromFavorites(SteamUtils.GetAppID(), (PublishedFileId_t)280762427); // http://steamcommunity.com/sharedfiles/filedetails/?id=280762427
+			OnUserFavoriteItemsListChangedCallResult.Set(handle);
+			print("SteamUGC.RemoveItemFromFavorites(" + SteamUtils.GetAppID() + ", (PublishedFileId_t)280762427) : " + handle);
+		}
+
+		if (GUILayout.Button("SubscribeItem((PublishedFileId_t)280762427)")) {
+			SteamAPICall_t handle = SteamUGC.SubscribeItem((PublishedFileId_t)280762427); // http://steamcommunity.com/sharedfiles/filedetails/?id=280762427
+			OnRemoteStorageSubscribePublishedFileResultCallResult.Set(handle);
+			print("SteamUGC.SubscribeItem((PublishedFileId_t)280762427) : " + handle);
+		}
+
+		if (GUILayout.Button("UnsubscribeItem((PublishedFileId_t)280762427)")) {
+			SteamAPICall_t handle = SteamUGC.UnsubscribeItem((PublishedFileId_t)280762427); // http://steamcommunity.com/sharedfiles/filedetails/?id=280762427
 			OnRemoteStorageUnsubscribePublishedFileResultCallResult.Set(handle);
-			print("SteamUGC. : " + handle);
+			print("SteamUGC.UnsubscribeItem((PublishedFileId_t)280762427) : " + handle);
 		}
 
 		GUILayout.Label("GetNumSubscribedItems() : " + SteamUGC.GetNumSubscribedItems());
@@ -181,6 +258,8 @@ public class SteamUGCTest : MonoBehaviour {
 			m_PublishedFileId = PublishedFileID[0];
 			print("SteamUGC.GetSubscribedItems(" + PublishedFileID + ", (uint)PublishedFileID.Length) : " + ret + " -- " + PublishedFileID[0]);
 		}
+
+		GUILayout.Label("GetItemState(PublishedFileID) : " + (EItemState)SteamUGC.GetItemState(m_PublishedFileId));
 
 		{
 			ulong SizeOnDisk;
@@ -195,6 +274,10 @@ public class SteamUGCTest : MonoBehaviour {
 			ulong BytesTotal;
 			bool ret = SteamUGC.GetItemDownloadInfo(m_PublishedFileId, out BytesDownloaded, out BytesTotal);
 			GUILayout.Label("GetItemDownloadInfo(m_PublishedFileId, out BytesDownloaded, out BytesTotal) : " + ret + " -- " + BytesDownloaded + " -- " + BytesTotal);
+		}
+
+		if (GUILayout.Button("DownloadItem(PublishedFileID, true)")) {
+			print("SteamUGC.DownloadItem(m_PublishedFileId, true) : " + SteamUGC.DownloadItem(m_PublishedFileId, true));
 		}
 	}
 
@@ -218,6 +301,14 @@ public class SteamUGCTest : MonoBehaviour {
 
 	void OnItemInstalled(ItemInstalled_t pCallback) {
 		Debug.Log("[" + ItemInstalled_t.k_iCallback + " - ItemInstalled_t] - " + pCallback.m_unAppID + " -- " + pCallback.m_nPublishedFileId);
+	}
+
+	void OnDownloadItemResult(DownloadItemResult_t pCallback) {
+		Debug.Log("[" + DownloadItemResult_t.k_iCallback + " - DownloadItemResult_t] - " + pCallback.m_unAppID + " -- " + pCallback.m_nPublishedFileId + " -- " + pCallback.m_eResult);
+	}
+
+	void OnUserFavoriteItemsListChanged(UserFavoriteItemsListChanged_t pCallback, bool bIOFailure) {
+		Debug.Log("[" + UserFavoriteItemsListChanged_t.k_iCallback + " - UserFavoriteItemsListChanged_t] - " + pCallback.m_nPublishedFileId + " -- " + pCallback.m_eResult + " -- " + pCallback.m_bWasAddRequest);
 	}
 
 	// These are from ISteamRemoteStorage, but also used here.
